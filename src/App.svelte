d3ca1786b09629fd605388bff82c2551b6902178
hemanth-hk <uppadahemanthkumar27@gmail.com>
2020-01-09 19:50:13 +0530

    Basic setup

---
+++ src/App.svelte
@@ -0,0 +1,107 @@
+<script>
+	import Screen from './screen.svelte'
+	let equation = 'ODE'
+
+</script>
+
+<main>
+<section class="hero is-light">
+  <div class="hero-head">
+    <nav class="navbar">
+      <div class="container">
+		<div class="title">
+			<h1>Numerical Analyzer</h1>
+		</div>
+        <div class="navbar-menu">
+          <div class="navbar-end">
+            <span class="navbar-item">
+              <a href="!#" class="button is-size-5">
+                <span class="icon">
+                  <i class="fab fa-github"></i>
+                </span>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</section>
+<br>
+  <div class="container">
+<div class="columns">
+  <div class="column is-4">
+  <span class="label">Equation :</span>
+<div class="select is-primary">
+  <select bind:value={equation}>
+    <option value="ODE">Ordinary Differential Equation</option>
+    <option value="PDE">Partial Differential Equation</option>
+  </select>
+</div>
+  </div>
+  <div class="column is-5">
+  <span class="label">Method :</span>
+<div class="select is-primary">
+	{#if equation == 'ODE' }
+		   <select>
+				<option>Euler's Forward Method</option>
+				<option>Heun's Method</option>
+				<option>Polygon Method (Modified Euler's Method)</option>
+				<option>Ralston's Method (RK-2 Method)</option>
+				<option>Runge-Kutta Method (4th Order Method)</option>
+			</select>
+	{:else}
+		<select>
+			<option>Partial Differential Equation</option>
+			<option>Heun's Method</option>
+		</select>
+	{/if}
+</div>
+</div>
+  <div class="column is-2">
+<div class="field">
+    <span class="label">Step Size (h):</span>
+  <div class="field-body">
+    <div class="field">
+      <p class="control">
+        <input class="input is-primary" type="number" placeholder="Step Size">
+      </p>
+    </div>
+  </div>
+  </div>
+</div>
+  </div>
+  <br>
+  <div class="container">
+  <div class="columns">
+  <div class="column is-7 is-offset-2">
+  <div class="field is-horizontal">
+  <div class="field-label is-normal">
+    <label class="label">dy/dx = </label>
+  </div>
+  <div class="field-body">
+    <div class="field">
+      <p class="control">
+        <input class="input is-primary" type="text" placeholder="Function in x and y or f(x,y)">
+      </p>
+    </div>
+  </div>
+  	<div class="buttons has-addons is-centered">
+		<button class="button is-primary is-light">Let's Do Some Math</button>
+	</div>
+  </div>
+  </div>
+  </div>
+<br>
+<Screen />
+</main>
+
+<style>
+.title h1{
+	margin-top: 1rem;
+	font-size: 1.2em;
+}
+.has-addons{
+	margin-left: 1em;
+}
+</style>
---
+++ src/screen.svelte
@@ -0,0 +1,103 @@
+<script>
+</script>
+
+<main class="container" id="screen">
+<div class="columns">
+<div class="column here has-background-light">
+<section class="hero is-fullheight">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Fullheight title
+      </h1>
+      <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+    </div>
+  </div>
+</section>
+</div>
+
+<div class="column here has-background-light">
+<section class="hero is-fullheight">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Fullheight title
+      </h1>
+      <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+    <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+      <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+      <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+      <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+      <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, id accusamus beatae dolor, voluptatum totam exercitationem impedit nulla reiciendis magni autem repellat magnam porro quae dolorum alias sint quaerat maiores?</p>
+      <br>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe aperiam incidunt ad quibusdam, natus exercitationem quisquam maiores nesciunt numquam adipisci atque distinctio autem, eveniet molestiae sunt culpa odit vitae. Qui?</p>
+            <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+            <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+            <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+            <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+            <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+            <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+            <h2 class="subtitle">
+        Fullheight subtitle
+      </h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis deserunt enim perferendis suscipit voluptatibus magnam facere quo. Dolorem quasi, quis iure nesciunt accusantium blanditiis cumque tenetur eius beatae consequatur provident!</p>
+    </div>
+  </div>
+</section>
+</div>
+</div>
+</main>
+
+<style>
+.here{
+    overflow-y: scroll;
+    overflow-x: hidden;
+    max-height: 96vh;
+}
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: rgb(161, 161, 161); 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
+</style>
